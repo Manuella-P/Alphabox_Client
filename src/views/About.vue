b7961a3622bca0ca017583428cdc1af774175770
Jules <julesjason955@gmail.com>
2021-06-03 18:42:38 +0100

    initialisation du project alphabox client

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about" style=".about{background:blue;}">
+    <h1>This is an about page</h1>
+    <div>
+      
+
+      <b-button href="#">I am a Link</b-button>
+    </div>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+
+    <h1> Welcome to Alphabox Game
+    </h1>
+   <b-button href="#" style="center">Play</b-button>
+  </div>
+</template>
+
+// <script>
+// // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'Home',
+//   components: {
+//     HelloWorld
+//   }
+// }
+// </script>
